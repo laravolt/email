e829b3071549750cb0c62bd57b205e9e10921348
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2015-10-19 04:46:49 +0000

    first commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Set specific configuration variables here
+ */
+return [
+    // automatic loading of routes through main service provider
+    'routes' => true,
+
+    // email view
+    'activation' => 'email::activation'
+];
